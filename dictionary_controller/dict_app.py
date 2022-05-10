fd9504d22e7aafbe99551f6c0a77df1c06eaad8d
akshaya <akshayaakshaya2000@gmail.com>
2022-05-10 23:13:55 +0530

    Add data.json file after making a data directory Add dict_app.py file dictionary_controller folder

---
+++ dictionary_controller/dict_app.py
@@ -0,0 +1,19 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open(r"C:\Users\EQ SENSE\PycharmProjects\aiprofile\data\data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
