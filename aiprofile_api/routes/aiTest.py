77bb38ba5f98fac86735f31db88a839fc300c26e
Vishal <princejaineet.000@gmail.com>
2022-05-16 21:36:08 +0530

    added one PR for divide

--- aiprofile_api/routes/aiTest.py
+++ aiprofile_api/routes/aiTest.py
@@ -31,3 +31,6 @@ def rohit():
         return jsonify({"result":payload["num1"]-payload["num2"]})
     elif payload["operation"]=="mul":
         return jsonify({"result":payload["num1"]*payload["num2"]})
+    elif payload["operation"] == "div":
+        return jsonify({"result": payload["num1"] / payload["num2"]})
+    
