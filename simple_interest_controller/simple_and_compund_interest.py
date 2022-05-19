1365b3df1152e9e6badb9f7e1118730e76e51e96
ankit <ankitverma2776@gmail.com>
2022-05-19 21:17:29 +0530

    add_four_numbers_and_multiply

--- simple_interest_controller/simple_and_compund_interest.py
+++ simple_interest_controller/simple_and_compund_interest.py
@@ -8,3 +8,17 @@ def compound_find_interest(p2, r2, t2):
     c_interest = p2 * (pow((1 + r2 / 100), t2))
     total_amount = p2 + c_interest
     return c_interest, total_amount
+
+def find_prime_number(payload):
+    start = payload["start"]
+    stop = payload["stop"]
+    prime_lst = []
+    for val in range(start, stop):
+        if val > 1:
+            for i in range(2, val):
+                if (val % i) == 0:
+                    break
+            else:
+                print(val, end=" ")
+                prime_lst.append(val)
+    return prime_lst
